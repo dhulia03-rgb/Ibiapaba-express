9f37d248b3c774eddc9580aaccba5f326b6bb192
Dhulia003 <dhulia03@gmail.com>
2026-09-26 06:54:11 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import { Home } from './components/Home';
+
 export default function App() {
-  return (
-    <div style={{ padding: '20px', fontSize: '24px', color: 'black' }}>
-      <h1>App funcionando!</h1>
-      <p>Se você está vendo isso, o React está ok.</p>
-    </div>
-  );
+  return <Home />;
 }
